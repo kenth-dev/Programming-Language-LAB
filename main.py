14cbbc737e8fbea841a398b21c730395a53f61e7
Kenneth Gasmen <kengasmen@gmail.com>
2026-04-22 09:02:23 +0800

    Add call to semantic test in main execution flow

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from test_lexer import run_lexer_test
 from test_parser import run_parser_test
+from test_semantic import run_semantic_test
+
 
 if __name__ == "__main__":
     run_lexer_test()
     print()
     run_parser_test()
+    print()
+    run_semantic_test()
 
